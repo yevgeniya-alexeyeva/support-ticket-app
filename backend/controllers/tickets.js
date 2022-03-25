f791f55a53ab90b0b15ca638f41f8427d02b5581
Yevgeniya Alexeyeva <yevgeniyaalexeyeva@noviprog.com>
2022-03-25 14:41:59 +0100

    add update-delete-ticket routs

--- backend/controllers/tickets.js
+++ backend/controllers/tickets.js
@@ -4,13 +4,39 @@ const { User, Ticket } = require("../model");
 const getTickets = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user.id);
   if (!user) {
-    res.status(400).json({
-      code: 400,
+    res.status(401).json({
+      code: 401,
       message: "User not found",
     });
   }
   const tickets = await Ticket.find({ user: req.user.id });
   res.status(200).json(tickets);
+});
+
+const getTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401).json({
+      code: 401,
+      message: "User not found",
+    });
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404).json({
+      code: 404,
+      message: "Ticket not found",
+    });
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401).json({
+      code: 401,
+      message: "Not Authorized",
+    });
+  }
+  res.status(200).json(ticket);
 });
 
 const createTicket = asyncHandler(async (req, res) => {
@@ -42,7 +68,72 @@ const createTicket = asyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
+const deleteTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401).json({
+      code: 401,
+      message: "User not found",
+    });
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404).json({
+      code: 404,
+      message: "Ticket not found",
+    });
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401).json({
+      code: 401,
+      message: "Not Authorized",
+    });
+  }
+
+  await ticket.remove();
+
+  res.status(200).json({ success: true });
+});
+
+const updateTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401).json({
+      code: 401,
+      message: "User not found",
+    });
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404).json({
+      code: 404,
+      message: "Ticket not found",
+    });
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401).json({
+      code: 401,
+      message: "Not Authorized",
+    });
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedTicket);
+});
+
 module.exports = {
   getTickets,
   createTicket,
+  getTicket,
+  deleteTicket,
+  updateTicket,
 };
--- backend/routes/tickets.js
+++ backend/routes/tickets.js
@@ -1,8 +1,20 @@
 const router = require("express").Router();
 const { protect } = require("../middleware");
 
-const { getTickets, createTicket } = require("../controllers/tickets");
+const {
+  getTickets,
+  createTicket,
+  getTicket,
+  deleteTicket,
+  updateTicket,
+} = require("../controllers/tickets");
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = { router };
